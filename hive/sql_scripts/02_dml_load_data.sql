0c9760a2f95a09df7cee933621966d77bb6bf160
张齐涛 <2986294284@qq.com>
2026-04-16 14:57:35 +0800

    Update 02_dml_load_data.sql

--- hive/sql_scripts/02_dml_load_data.sql
+++ hive/sql_scripts/02_dml_load_data.sql
@@ -1,8 +1,24 @@
 -- 1. 从本地加载数据
-LOAD DATA LOCAL INPATH '/opt/module/hive/data/product_info.txt' INTO TABLE product_info;
+LOAD DATA LOCAL INPATH 'product_info.txt' INTO TABLE product_info;
 
 -- 2. 动态分区插入 (重点)
 SET hive.exec.dynamic.partition=true;
 SET hive.exec.dynamic.partition.mode=nonstrict;
 INSERT OVERWRITE TABLE log_partition PARTITION(day)
 SELECT url, day FROM raw_log;
+
+-- 1. 商品信息表 (对应 product_info.txt)
+CREATE TABLE IF NOT EXISTS product_info(
+    id INT COMMENT '商品id',
+    name STRING COMMENT '商品名称',
+    category_id INT COMMENT '分类id',
+    price DECIMAL(10,2) COMMENT '价格'
+)
+ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t';
+
+-- 2. 省份信息表 (对应 province_info.txt)
+CREATE TABLE IF NOT EXISTS province_info(
+    id INT COMMENT '省份id',
+    name STRING COMMENT '省份名称'
+)
+ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t';
